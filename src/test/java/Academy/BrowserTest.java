b8a966a9e5faceaf9a5bf8e34b65b9d9c08a581f
Pathak101 <93067061+Pathak101@users.noreply.github.com>
2025-12-23 22:48:13 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -17,12 +17,21 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys");
-		WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+		
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://siddharthpathak.azurewebsites.net/webapp/");
+
+        Thread.sleep(5000);
 		// System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		// WebDriver driver =new ChromeDriver();
-		driver.get("https://siddharthpathak.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.xpath("//h1[normalize-space()='RahulShettyAcademy.com Learning']")).getText();
 		System.out.println(text);
 		// Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
